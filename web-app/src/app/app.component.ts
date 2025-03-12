b03b5bf1d53e1abd91dbef39a1b95f4cf5611645
hredan <hredan@sleepuino.de>
2025-03-12 08:38:53 +0000

    add interface Core, but seems not working currently

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import * as coreList_input from '../../public/core_list.json';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'ESP-Board Overview';
+  coreList: Core[] = coreList_input;
+  ngOnInit() {
+    console.log(this.coreList);
+  }
+}
+
+export interface Core {
+  core: string;
+  installed_version: string;
+  latest_version: string;
+  core_name: string;
 }
