877ab2ba6bf5aca2381017ec55543bc5f15ea82f
hredan <hredan@sleepuino.de>
2025-04-04 06:31:10 +0000

    read esp8266.csv

--- web-app/src/app/board-overview/board-overview.component.ts
+++ web-app/src/app/board-overview/board-overview.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
+import { HttpClient} from '@angular/common/http'; 
 
 @Component({
   selector: 'app-board-overview',
@@ -7,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrl: './board-overview.component.css'
 })
 export class BoardOverviewComponent {
-
+  csvData: string = '';
+  csvRecords: any;
+  constructor(private ngxCsvParser: NgxCsvParser, private httpClient: HttpClient) { }
+  ngOnInit() {
+    this.getCsvData();
+    //this.loadCSV(file);
+  }
+  getCsvData() {
+    const csvFilePath = './esp8266.csv';
+    this.httpClient
+      .get(csvFilePath, { responseType: 'text' })
+      .subscribe((data) => {
+        this.csvData = data;
+        console.log(this.csvData);
+      });
+  }
+  loadCSV(csvFile: File) {
+    
+    this.ngxCsvParser.parse(csvFile, { header: true, delimiter: ',' })
+    .pipe().subscribe({
+      next: (result): void => {
+        console.log('Result', result);
+        this.csvRecords = result;
+      },
+      error: (error: NgxCSVParserError): void => {
+        console.log('Error', error);
+      }
+    });
+  }
 }
