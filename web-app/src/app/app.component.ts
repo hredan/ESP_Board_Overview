402266eb4943e647436938156bceab8421154aaa
hredan <hredan@sleepuino.de>
2025-04-04 06:30:46 +0000

    read core json in board-overview

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -3,8 +3,6 @@ import { RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
 import { ChildrenOutletContexts } from '@angular/router';
 // import { AppRoutingModule } from './app.routes';
 
-import * as coreList_input from '../../public/core_list.json';
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLinkActive, RouterLink],
@@ -18,18 +16,4 @@ export class AppComponent {
   getRouteAnimationData() {
     return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
   }
-
-  coreList: Core[] = [];
-  ngOnInit() {
-    this.coreList = (coreList_input as any).default;
-    console.log(this.coreList.length);
-    console.log(this.coreList[1].core);
-  }
-}
-
-export interface Core {
-  core: string;
-  installed_version: string;
-  latest_version: string;
-  core_name: string;
 }
--- web-app/src/app/esp-core-overview/esp-core-overview.component.ts
+++ web-app/src/app/esp-core-overview/esp-core-overview.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import * as coreList_input from '../../../public/core_list.json';
 
 @Component({
   selector: 'app-esp-core-overview',
@@ -7,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './esp-core-overview.component.css'
 })
 export class EspCoreOverviewComponent {
+  coreList: Core[] = [];
+  ngOnInit() {
+    this.coreList = (coreList_input as any).default;
+    console.log(this.coreList.length);
+    console.log(this.coreList[1].core);
+  }
+}
 
+export interface Core {
+  core: string;
+  installed_version: string;
+  latest_version: string;
+  core_name: string;
 }
