31b4090818f76c8509c2bed12ea68a91c0c49a38
hredan <hredan@sleepuino.de>
2025-04-04 03:25:47 +0000

    add info tap

--- web-app/src/app/app.routes.ts
+++ web-app/src/app/app.routes.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BoardOverviewComponent } from './board-overview/board-overview.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { EspCoreOverviewComponent } from './esp-core-overview/esp-core-overview.component';
 
 export const routes: Routes = [
+    { path: 'info', component: EspCoreOverviewComponent },
     { path: 'esp8266', component: BoardOverviewComponent },
     { path: '',   redirectTo: '/esp8266', pathMatch: 'full' }, // redirect to `trend`
     { path: '**', component: PageNotFoundComponent },
 ];
-
-// @NgModule({
-//     imports: [RouterModule.forChild(routes)],
-//     exports: [RouterModule]
-//   })
-// export class AppRoutingModule { }
---
+++ web-app/src/app/esp-core-overview/esp-core-overview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-esp-core-overview',
+  imports: [],
+  templateUrl: './esp-core-overview.component.html',
+  styleUrl: './esp-core-overview.component.css'
+})
+export class EspCoreOverviewComponent {
+
+}
