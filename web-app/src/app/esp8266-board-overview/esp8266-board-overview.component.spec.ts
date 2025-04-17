bac898bb099d87d1d680ad86dc143620f01efe7d
hredan <hredan@sleepuino.de>
2025-04-17 13:31:06 +0000

    add ng-mocks and use it for board-overview-component

--- web-app/src/app/esp8266-board-overview/esp8266-board-overview.component.spec.ts
+++ web-app/src/app/esp8266-board-overview/esp8266-board-overview.component.spec.ts
@@ -1,34 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
-import { provideHttpClient } from '@angular/common/http';
+import { MockComponent } from 'ng-mocks';
 
 import { Esp8266BoardOverviewComponent } from './esp8266-board-overview.component';
-
-
+import { BoardOverviewComponent } from '../board-overview/board-overview.component';
+import { By } from '@angular/platform-browser';
 
 describe('Esp8266BoardOverviewComponent', () => {
   let component: Esp8266BoardOverviewComponent;
   let fixture: ComponentFixture<Esp8266BoardOverviewComponent>;
+  let boardOverview: BoardOverviewComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      providers: [
-        provideHttpClient(),
-        provideHttpClientTesting()
-      ],
-    });
-
-    await TestBed.configureTestingModule({
+      declarations: [Esp8266BoardOverviewComponent, MockComponent(BoardOverviewComponent)],
       imports: [Esp8266BoardOverviewComponent]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Esp8266BoardOverviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    const boardOverviewComponent = fixture.debugElement.query(By.directive(BoardOverviewComponent));
+    boardOverview = boardOverviewComponent.componentInstance;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('rendered board overview component', () => {
+    expect(boardOverview).toBeTruthy();
+  });
+
+  it('should set the board type to ESP8266', () => {
+    expect(boardOverview.coreName).toEqual('esp8266');
   });
 });
