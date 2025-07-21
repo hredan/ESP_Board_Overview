0b8f882bc24ee11aef2aa6c1a54be2f1cd60ea6b
hredan <hredan@sleepuino.de>
2025-07-21 19:45:08 +0000

    change repo name to esp-board-overview

--- Scripts/deploy_web-app.sh
+++ Scripts/deploy_web-app.sh
@@ -2,4 +2,4 @@
 # This script deploys the web app to the docs folder for GitHub Pages.
 # It is designed to be run in a Linux environment, specifically Ubuntu 22.04.
 cd /workspaces/ESP_Board_Overview/web-app/
-ng deploy --base-href "https://hredan.github.io/ESP_Board_Overview/"
+ng deploy --base-href "https://hredan.github.io/esp-board-overview/"
