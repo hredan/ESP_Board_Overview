c40e625335cc9098b44b507f4d828fc57249c490
hredan <hredan@sleepuino.de>
2025-07-21 19:35:54 +0000

    update core esp32 to version 3.2.1

--- Scripts/copy_data2public.sh
+++ Scripts/copy_data2public.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cp /workspaces/ESP_Board_Overview/core_list.json /workspaces/ESP_Board_Overview/web-app/data/
-cp /workspaces/ESP_Board_Overview/esp32.json /workspaces/ESP_Board_Overview/web-app/data/
-cp /workspaces/ESP_Board_Overview/esp8266.json /workspaces/ESP_Board_Overview/web-app/data/
+cp /workspaces/ESP_Board_Overview/esp_data/core_list.json /workspaces/ESP_Board_Overview/web-app/data/
+cp /workspaces/ESP_Board_Overview/esp_data/esp32.json /workspaces/ESP_Board_Overview/web-app/data/
+cp /workspaces/ESP_Board_Overview/esp_data/esp8266.json /workspaces/ESP_Board_Overview/web-app/data/
